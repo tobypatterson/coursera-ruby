d1a070980a04400617a3cb47baba36983b2ea9bf
Toby Patterson <toby.patterson@gmail.com>
2016-12-11 14:42:30 -0500

    assignment 1

---
+++ angular/assignment1/app.js
@@ -0,0 +1,38 @@
+(function(){
+	'use strict';
+
+	angular.module('LunchCheck', [])
+	.controller('LunchCheckController', LunchCheckController);
+
+	LunchCheckController.$inject = ['$scope'];
+
+	function LunchCheckController($scope) {
+		$scope.dishes = '';
+		$scope.message = '';
+		$scope.feedback = 'unknown';
+
+		$scope.updateMessage = function() 
+		{
+			var message = 'Please enter data first';
+			var feedback = 'bad';
+
+			if ($scope.dishes) {
+				var notEmpty = function(v) { return v && v.replace(/ /g,'').length};
+				var count = $scope.dishes.split(',').filter(notEmpty).length
+				console.log(message, count)
+
+				if (count <= 3) {
+					message = 'Enjoy!';
+				} else {
+					message = 'Too much!'
+				}
+
+				feedback = 'good';
+			}
+
+			$scope.message = message;
+			$scope.feedback = feedback
+		}
+
+	}
+})()
